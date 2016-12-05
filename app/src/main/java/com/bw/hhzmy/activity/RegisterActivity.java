b6f34405718734f091732a0529aafa305c935783
zhuyaojing2333 <925749557@qq.com>
2016-12-05 20:43:42 +0800

    登录界面

--- app/src/main/java/com/bw/hhzmy/activity/RegisterActivity.java
+++ app/src/main/java/com/bw/hhzmy/activity/RegisterActivity.java
@@ -1,14 +1,18 @@
 package com.bw.hhzmy.activity;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bw.hhzmy.util.CodeUtils;
+import com.bw.hhzmy.util.Validator;
 import com.bw.hhzmy.view.ClearEditText;
 
 import butterknife.Bind;
@@ -29,6 +33,8 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     @Bind(R.id.next_bu)
     Button nextBu;
     private CodeUtils codeUtils;
+    private String phoneStr;
+    private String codeStr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +48,7 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
 
         ivBack.setOnClickListener(this);
         yanzhengImg.setOnClickListener(this);
+        nextBu.setOnClickListener(this);
     }
 
     @Override
@@ -54,6 +61,30 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
                 codeUtils = CodeUtils.getInstance();
                 Bitmap bitmap = codeUtils.createBitmap();
                 yanzhengImg.setImageBitmap(bitmap);
+                break;
+            case R.id.next_bu:
+                phoneStr = phoneNumber.getText().toString();
+                codeStr = yanzhengCode.getText().toString().trim();
+
+                if(Validator.isMobile(phoneStr)){
+                    String code = codeUtils.getCode();
+                    if (code.equalsIgnoreCase(codeStr)) {
+                        if(cbChoose.isChecked()){
+                            Intent intent = new Intent(RegisterActivity.this, MessageActivity.class);
+
+                            startActivity(intent);
+                        }else{
+                            Toast.makeText(this, "请同意苏宁易购会员章程和易付宝协议后再进行下一步操作", Toast.LENGTH_LONG).show();
+                        }
+                    } else {
+                        Toast.makeText(this, "验证码错误", Toast.LENGTH_LONG).show();
+                    }
+                }else if(null == phoneStr || TextUtils.isEmpty(phoneStr)) {
+                    Toast.makeText(this, "手机号不能为空", Toast.LENGTH_LONG).show();
+                }else {
+                    Toast.makeText(this, "请输入正确的手机号码", Toast.LENGTH_LONG).show();
+                }
+
                 break;
         }
     }
